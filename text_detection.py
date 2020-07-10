22690c468896c25ba31789b2becbc687a7bd858f
Ankit Saxena <Ankit.Saxena@mail.weir>
2020-07-11 00:05:41 +0530

    added some modularity

--- text_detection.py
+++ text_detection.py
@@ -22,76 +22,85 @@ def get_arguments():
     return arguments
 
 
-args = get_arguments()
+def forward_passer(detector, layers, image):
+
+    print("[INFO] loading the detector...")
+    net = cv2.dnn.readNet(detector)
+
+    h, w = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1.0, (w, h), (123.68, 116.78, 103.94), swapRB=True, crop=False)
+    start = time.time()
+    net.setInput(blob)
+    scores, geometry = net.forward(layers)
+    end = time.time()
+
+    print(f"[INFO] detection in {round(end - start, 2)} seconds")
+
+    return scores, geometry
 
 
-image = cv2.imread(args['image'])
-orig_image = image.copy()
-h, w = image.shape[:2]
+if __name__ == '__main__':
 
-new_w, new_h = args['width'], args['height']
-ratio_w = w / new_w
-ratio_h = h / new_h
+    args = get_arguments()
 
-image = cv2.resize(image, (new_w, new_h))
-h, w = image.shape[:2]
+    image = cv2.imread(args['image'])
+    orig_image = image.copy()
+    h, w = image.shape[:2]
 
-layer_names = ['feature_fusion/Conv_7/Sigmoid',
-               'feature_fusion/concat_3']
+    new_w, new_h = args['width'], args['height']
+    ratio_w = w / new_w
+    ratio_h = h / new_h
 
-print("[INFO] loading the detector...")
-net = cv2.dnn.readNet(args['east'])
+    image = cv2.resize(image, (new_w, new_h))
+    h, w = image.shape[:2]
 
-blob = cv2.dnn.blobFromImage(image, 1.0, (w, h), (123.68, 116.78, 103.94), swapRB=True, crop=False)
-start = time.time()
-net.setInput(blob)
-scores, geometry = net.forward(layer_names)
-end = time.time()
+    layer_names = ['feature_fusion/Conv_7/Sigmoid',
+                   'feature_fusion/concat_3']
 
-print(f"[INFO] detection in {round(end - start, 2)} seconds")
+    scores, geometry = forward_passer(detector=args['east'], layers=layer_names, image=image)
 
-num_rows, num_cols = scores.shape[2:4]
-rectangles = []
-confidences = []
+    num_rows, num_cols = scores.shape[2:4]
+    rectangles = []
+    confidences = []
 
-for y in range(num_rows):
-    scores_data = scores[0, 0, y]
-    x_data0 = geometry[0, 0, y]
-    x_data1 = geometry[0, 1, y]
-    x_data2 = geometry[0, 2, y]
-    x_data3 = geometry[0, 3, y]
-    angles_data = geometry[0, 4, y]
+    for y in range(num_rows):
+        scores_data = scores[0, 0, y]
+        x_data0 = geometry[0, 0, y]
+        x_data1 = geometry[0, 1, y]
+        x_data2 = geometry[0, 2, y]
+        x_data3 = geometry[0, 3, y]
+        angles_data = geometry[0, 4, y]
 
-    for x in range(num_cols):
-        if scores_data[x] < args['min_confidence']:
-            continue
+        for x in range(num_cols):
+            if scores_data[x] < args['min_confidence']:
+                continue
 
-        offset_x, offset_y = x * 4.0, y * 4.0
+            offset_x, offset_y = x * 4.0, y * 4.0
 
-        angle = angles_data[x]
-        cos = np.cos(angle)
-        sin = np.sin(angle)
+            angle = angles_data[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
 
-        box_h = x_data0[x] + x_data2[x]
-        box_w = x_data1[x] + x_data3[x]
+            box_h = x_data0[x] + x_data2[x]
+            box_w = x_data1[x] + x_data3[x]
 
-        end_x = int(offset_x + (cos * x_data1[x]) + (sin * x_data2[x]))
-        end_y = int(offset_y + (cos * x_data2[x]) - (sin * x_data1[x]))
-        start_x = int(end_x - box_w)
-        start_y = int(end_y - box_h)
+            end_x = int(offset_x + (cos * x_data1[x]) + (sin * x_data2[x]))
+            end_y = int(offset_y + (cos * x_data2[x]) - (sin * x_data1[x]))
+            start_x = int(end_x - box_w)
+            start_y = int(end_y - box_h)
 
-        rectangles.append((start_x, start_y, end_x, end_y))
-        confidences.append(scores_data[x])
+            rectangles.append((start_x, start_y, end_x, end_y))
+            confidences.append(scores_data[x])
 
-boxes = non_max_suppression(np.array(rectangles), probs=confidences)
+    boxes = non_max_suppression(np.array(rectangles), probs=confidences)
 
-for (start_x, start_y, end_x, end_y) in boxes:
-    start_x = int(start_x * ratio_w)
-    start_y = int(start_y * ratio_h)
-    end_x = int(end_x * ratio_w)
-    end_y = int(end_y * ratio_h)
+    for (start_x, start_y, end_x, end_y) in boxes:
+        start_x = int(start_x * ratio_w)
+        start_y = int(start_y * ratio_h)
+        end_x = int(end_x * ratio_w)
+        end_y = int(end_y * ratio_h)
 
-    cv2.rectangle(orig_image, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+        cv2.rectangle(orig_image, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
 
-cv2.imshow("Detection", orig_image)
-cv2.waitKey(0)
+    cv2.imshow("Detection", orig_image)
+    cv2.waitKey(0)
